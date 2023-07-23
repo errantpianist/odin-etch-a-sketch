8625b076c95e6d3657b62c81e9fe636014f482d1
Yu Zhou Zhang <yuzspanish@gmail.com>
2023-07-23 13:36:58 +0100

    add rainbow button

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const grid = document.getElementById("grid");
-createGrid(16);
+createGrid(16, onHoverRainbow);
 
 const clearBtn = document.getElementById("clear");
 clearBtn.addEventListener("click", clearGrid);
+
+const blackBtn = document.getElementById("black");
+blackBtn.addEventListener("click", () => {
+  blackBtn.classList.add("active");
+  rainbowBtn.classList.remove("active");
+  clearGrid();
+  createGrid(sizeSlider.value, onHoverBlack);
+});
+
+const rainbowBtn = document.getElementById("rainbow");
+rainbowBtn.addEventListener("click", () => {
+  rainbowBtn.classList.add("active");
+  blackBtn.classList.remove("active");
+  clearGrid();
+  createGrid(sizeSlider.value, onHoverRainbow);
+});
 
 const sizeSlider = document.getElementById("size");
 const sizeSliderValue = document.getElementById("size-slider-value");
@@ -11,17 +27,14 @@ sizeSlider.addEventListener("input", (e) => {
 });
 sizeSlider.addEventListener("change", (e) => {
   clearGrid();
-  createGrid(e.target.value);
+  createGrid(e.target.value, onHoverBlack);
 });
 
-function createGrid(size) {
-  const onHover = (e) => {
-    e.target.style.backgroundColor = "black";
-  };
+function createGrid(size, onHover) {
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.addEventListener("mouseover", onHover);
+    cell.onmouseover = onHover;
     grid.appendChild(cell);
   }
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -29,8 +42,19 @@ function createGrid(size) {
 }
 
 function clearGrid() {
-  const cells = document.querySelectorAll(".cell");
-  cells.forEach((cell) => {
-    cell.style.backgroundColor = "white";
-  });
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+}
+
+function onHoverBlack(e) {
+  e.target.style.backgroundColor = "black";
+}
+function onHoverRainbow(e) {
+  e.target.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + randomColor;
 }
